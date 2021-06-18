c35b326cdba8ed721c01aab689ea0ac9a22a7ee7
Jaimit <68295105+jaimit25@users.noreply.github.com>
2021-06-18 21:28:33 +0530

    Update Feed.dart

--- lib/Model/Feed.dart
+++ lib/Model/Feed.dart
@@ -1,5 +1,26 @@
 
-
+// static getdata() async {
+//     print('datadtadatdatdatdatdatadtadtadtadtadtadtadtad');
+//     List<FeedModel> list = [];
+//     await FirebaseFirestore.instance
+//         .collection("Feed")
+//         .get()
+//         .then((querySnapshot) {
+//       querySnapshot.docs.forEach((document) {
+//         list.add(FeedModel(
+//           email: document['email'],
+//           // Head: document['Head'],
+//           body: document['body'],
+//           likes: document['likes'],
+//           Photo: document['Photo'],
+//         ));
+//       });
+//     });
+//     print('xxx');
+//     print(list.length);
+//     print('yyy');
+//     return list;
+//   }
 // class Feed {
 //   String IntExtra, StringExtra, imgUrl, Caption, Name;
 //   bool likes;
